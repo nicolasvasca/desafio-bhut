3a9cb185896a21243a90e5def7ecbd045da9e06b
Nicolas Vasca <nicolasvasca@gmail.com>
2023-07-23 15:09:50 -0300

    test: create order

--- test/unit/webhooks.service.spec.ts
+++ test/unit/webhooks.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WebhooksService } from '../../src/aplication/services/webhooks.service';
+import MockWebHookGateway from './__mocks__/MockWebHook';
+import { IWebHookGateway } from '../../src/aplication/ports/IWebHookGateway';
+import MockHttpService from './__mocks__/MockHttpService';
+import { HttpService } from '@nestjs/axios';
+import MockCars from './__mocks__/MockCars';
 
 describe('WebhooksService', () => {
   let service: WebhooksService;
+  const mockWebHookGateway = MockWebHookGateway.mockWebHookGateway();
+  const mockHttpService = MockHttpService.mockHttpService();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WebhooksService],
+      providers: [
+        WebhooksService,
+        {
+          provide: IWebHookGateway,
+          useValue: mockWebHookGateway,
+        },
+        {
+          provide: HttpService,
+          useValue: mockHttpService,
+        },
+      ],
     }).compile();
 
     service = module.get<WebhooksService>(WebhooksService);
@@ -14,5 +31,13 @@ describe('WebhooksService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('When Car Create', () => {
+    it('should be create order', async () => {
+      const carDto = MockCars.mockCarDto();
+      await service.createOrder(carDto);
+      expect(mockWebHookGateway.sendNotification).toBeCalledTimes(1);
+    });
   });
 });
